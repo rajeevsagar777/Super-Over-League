de101860febf858ebcfeb2849b81902d33780b7f
rajeevsagar <rajeev22335@gmail.com>
2022-02-17 10:42:32 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="mainBg">
+    <h1 className="mainHeading">Super Over League</h1>
+    <div className="imageContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
